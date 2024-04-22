686be1b773b5c2550997fbe40b9bcf11b4a2cfad
msappel4 <167126884+msappel4@users.noreply.github.com>
2024-04-22 13:56:08 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+#include "ShoppingCart.h"
+#include "ItemToPurchase.h"
+
+void PrintMenu() {
+   /* Type your code here */
+   
+}
+
+void ExecuteMenu(char option, ShoppingCart& theCart) {
+   /* Type your code here */
+   
+}
+
+int main() {
+   /* Type your code here */
+    
+ 
+   return 0;
+}
