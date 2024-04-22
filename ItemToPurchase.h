192197c6f82e5e57f40c19f62cb6fefcc46c9ede
msappel4 <167126884+msappel4@users.noreply.github.com>
2024-04-22 13:58:44 -0500

    Update ItemToPurchase.h

--- ItemToPurchase.h
+++ ItemToPurchase.h
@@ -1,1 +1,29 @@
+#ifndef ITEM_TO_PURCHASE_H
+#define ITEM_TO_PURCHASE_H
 
+#include <string>
+using namespace std;
+
+class ItemToPurchase{
+   
+   private:
+      string itemName;
+      string itemDescription;
+      int itemPrice;
+      int itemQuantity;
+    
+   public:
+      ItemToPurchase();
+      ItemToPurchase(string itemName, string itemDescription, int itemPrice, int itemQuantity);
+      void SetName(string itemName);
+      string GetName();
+      void SetPrice(int itemPrice);
+      int GetPrice();
+      void SetQuantity(int itemQuantity);
+      int GetQuantity();
+      void SetDescription(string itemDescription);
+      string GetDescription();
+      void PrintItemCost();
+      void PrintItemDescription();
+};
+#endif
