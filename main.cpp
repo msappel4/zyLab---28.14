df1a1cb7311327d1073aa9ba949a99adc32b5cf1
msappel4 <167126884+msappel4@users.noreply.github.com>
2024-04-24 17:20:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@ using namespace std;
 #include "ItemToPurchase.h"
 
 void PrintMenu() {
-   /* Type your code here */
-   
+   cout << "MENU" << endl;
+   cout << "a - Add item to cart" << endl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change item quantity" << endl;
+   cout << "i - Output items' descriptions" << endl;
+   cout << "o - Output shopping cart" << endl;
+   cout << "q - Quit" << endl;  
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
@@ -16,8 +21,40 @@ void ExecuteMenu(char option, ShoppingCart& theCart) {
 }
 
 int main() {
-   /* Type your code here */
-    
+   cout << "Enter customer's name:" << endl;
+   string name;
+   cin >> name;
+   cout << "Enter today's date:" << endl;
+   string date;
+   cin >> date;
+   cout << "Customer name: " << name << endl;
+   cout << "Today's date: " << date << endl;
+   ShoppingCart cart = ShoppingCart(name, date);
+   
+   PrintMenu();
+   cout << endl;
+   cout << "Choose an option:";
+   char option;
+   cin >> option;
+   bool valid;
+   
+   while (option != 'q'){
+      
+      if (option == 'a' || option == 'd' || option == 'c' || option == 'i' || option == 'o' || option == 'q'){
+         valid = true;
+         if (option == 'q'){
+            break;
+         }
+         else {
+            ExecuteMenu (option, cart);
+         }
+      }
+      else {
+         valid = false;
+         cout << "Enter a valid choice: " << endl;
+         cin >> option;
+      }
+   }
  
    return 0;
 }
