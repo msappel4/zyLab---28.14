5e34110028e5b8e1ef11234eb6cec1370d4d149a
miablank27 <167340947+miablank27@users.noreply.github.com>
2024-04-28 18:17:27 -0500

    Update main.cpp
    implemented Step 3, fixed cin errors

--- main.cpp
+++ main.cpp
@@ -17,7 +17,21 @@ void ExecuteMenu(char option, ShoppingCart& theCart) {
 }
 
 int main() {
-   /* Type your code here */
+   // define variables
+   string name;
+   string date;
+   
+   // input name
+   cout << "Enter customer's name:" << endl;
+   getline(cin, name);
+   // input date
+   cout << "Enter today's date:" << endl;
+   getline(cin, date);
+   
+   // output name and date
+   cout << endl;
+   cout << "Customer name: " << name << endl;
+   cout << "Today's date: " << date << endl;
     
  
    return 0;
