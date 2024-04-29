1d8096bb6e6e7de905e80060e4c4f182bb9c7939
miablank27 <167340947+miablank27@users.noreply.github.com>
2024-04-28 19:56:46 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,19 +7,87 @@ using namespace std;
 // VS code
 
 void PrintMenu() {
-   /* Type your code here */
-   
+   cout << "MENU" << endl;
+   cout << "a - Add item to cart" << endl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change item quantity" << endl;
+   cout << "i - Output items' descriptions" << endl;
+   cout << "o - Output shopping cart" << endl;
+   cout << "q - Quit" << endl;
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
+   // define variables
+   string itemName;
+   string itemDescription;
+   int itemPrice;
+   int itemQuantity;
+   int newQuantity;
+   ItemToPurchase newItem;
+   ItemToPurchase modifiedItem;
    
+   
+   switch (option) {
+      case 'o':
+         cout << "OUTPUT SHOPPING CART" << endl;
+         theCart.PrintTotal();
+         break;
+      
+      case 'i':
+         theCart.PrintDescriptions();
+         break;
+      
+      case 'a':
+         
+         cout << "ADD ITEM TO CART" << endl;
+         cout << "Enter the item name:" << endl;
+         getline (cin, itemName);
+         newItem.SetName(itemName);
+         cout << "Enter the item description:" << endl;
+         getline (cin, itemDescription);
+         newItem.SetDescription(itemDescription);
+         cout << "Enter the item price:" << endl;
+         cin >> itemPrice;
+         newItem.SetPrice(itemPrice);
+         cout << "Enter the item quantity:" << endl;
+         cin >> itemQuantity;
+         newItem.SetQuantity(itemQuantity);
+         theCart.AddItem(newItem);
+         
+         break;
+      
+      case 'd':
+         cout << "REMOVE ITEM FROM CART" << endl;
+         cout << "Enter the name of the item to remove:" << endl;
+         getline (cin, itemName);
+         theCart.RemoveItem (itemName);
+         break;
+      
+      case 'c':
+         
+         cout << "CHANGE ITEM QUANTITY" << endl;
+         cout << "Enter the item name:" << endl;
+         getline (cin, itemName);
+         cout << "Enter the new quantity:" << endl;
+         cin >> newQuantity;
+         modifiedItem.SetQuantity(newQuantity);
+         theCart.ModifyItem (modifiedItem);
+         break;
+      
+      case 'q':
+         cout << "Quitting" << endl;
+         break;
+      
+      default:
+         cout << "Invalid option. Please try again." << endl;
+   }
 }
 
 int main() {
    // define variables
    string name;
    string date;
+   char option;
    
    // input name
    cout << "Enter customer's name:" << endl;
@@ -32,7 +100,24 @@ int main() {
    cout << endl;
    cout << "Customer name: " << name << endl;
    cout << "Today's date: " << date << endl;
-    
+   
+   // create ShoppingCart object
+   ShoppingCart myCart(name, date);
+   
+   // call PrintMenu
+   cout << endl;
+   PrintMenu();
+   cout << endl;
+   
+   // call for option input
+   cout << "Choose an option: " << endl;
+   option = getchar();
+   
+   // call ExecuteMenu
+   ExecuteMenu(option, myCart);
+   
+   
+   
  
    return 0;
 }
